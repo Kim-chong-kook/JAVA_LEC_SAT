c0818681809a4766edf535fc0e60dabb1c1d6208
KJK88 <hardbee88@gmail.com>
2022-06-25 16:20:01 +0900

    나의 첫 깃 커밋

---
+++ Day01/MyfirstClass.java
@@ -0,0 +1,24 @@
+		/* 오늘 첫날 2022.6.25 자바학습.
+		 * lodossw@gmail.com
+		 * 이종석 강사님과 함께.
+		 * 카톡 ID : lodosswkor
+		 */
+public class MyfirstClass {
+	// 프로그램 스타팅 포인트
+	/**
+	 * java Doc 주석.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("My First Class"); // My First Class를 출력한다.
+		System.out.println("하하하하");
+		System
+		.out
+		
+		.println("만세만세")
+		
+		
+		; // 문장의 끝은 ;로 약속한다. 한줄에 두 문장을 이어 쓸수가 있다.
+		System.out.println("하하하하");System.out.println("하하하하");System.out.println("하하하하");
+	}
+}
