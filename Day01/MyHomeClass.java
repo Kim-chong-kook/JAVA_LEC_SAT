8b311a3211af0f60aeee209ce9ea3e65be329741
Kim-chong-kook <hardbee88@gmail.com>
2022-06-30 19:37:23 +0900

    아직 홈에서 변경1이 반영되지 않았나요?

--- Day01/MyHomeClass.java
+++ Day01/MyHomeClass.java
@@ -3,6 +3,8 @@ public class MyHomeClass {
 	public static void main(String[] args) {
 		System.out.println("여기는 나의 편한 집입니다.");
 		// 집에서 작업하면, 시간이 걸리더라도 편하고 많이 작업할 수 있습니다.
+		// 단점도 있지요. 항상 일을 생각하고 있어야 한다는 점입니다. 그것이 장점일 수도 있습니다.
+		// 바로 창의적인 산물을 생성할 수 있으니까요.
 	}
 
 }
